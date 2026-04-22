7177450c370b4b0833c900c62d4ec2e85c4d9289
Simgenur İŞLER <simgenur.isler@gmail.com>
2026-04-22 23:19:11 +0300

    Update firebase.ts

--- frontend/src/shared/firebase/firebase.ts
+++ frontend/src/shared/firebase/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "FILL_ME",
-  authDomain: "FILL_ME",
-  projectId: "FILL_ME",
-  storageBucket: "FILL_ME",
-  messagingSenderId: "FILL_ME",
-  appId: "FILL_ME",
+  apiKey: "AIzaSyBhi671xnKRwgQzcb1g6qGDVLN1Bzje8kI",
+  authDomain: "gebelik-takip-projesi.firebaseapp.com",
+  projectId: "gebelik-takip-projesi",
+  storageBucket: "gebelik-takip-projesi.firebasestorage.app",
+  messagingSenderId: "120823291388",
+  appId: "1:120823291388:web:edd9d34788a0943b40e679",
 };
 
 export const isFirebaseConfigured = !Object.values(firebaseConfig).some((value) => value === "FILL_ME");
