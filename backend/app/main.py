a44d98f360850327f43c1242c49b5a5868368f7a
Simgenur İŞLER <simgenur.isler@gmail.com>
2026-06-13 05:48:09 +0300

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,5 +1,17 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+origins = [
+    "http://localhost:5173",
+    "https://gebe-takip-sistemi.vercel.app"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 from .routers.auth_router import router as auth_router
 from .routers.calendar_router import router as calendar_router
